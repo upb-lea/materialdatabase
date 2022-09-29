660f94c7e02792a84dfcbd2188fc82c53ec86155
Aniket Singh <100148478+Aniketsingh17@users.noreply.github.com>
2022-09-29 18:50:32 +0200

    channges in classes file

--- materialdatabase/material_data_base_classes.py
+++ materialdatabase/material_data_base_classes.py
@@ -205,6 +205,44 @@ class MaterialDatabase:
         print(f"Material properties {properties} of {material_name} are plotted.")
         pass
 
+    # --------to get different material property from database file---------
+    @staticmethod
+    def get_material_property(material_name: str, property: str):
+        """
+            :param material_name: str: N95,N87.....
+            :param property: str:  initial_permeability, resistivity, max_flux_density, weight_density
+        """
+        script_dir = os.path.dirname(__file__)
+        file_path = os.path.join(script_dir, 'data/material_data_base.json')
+        with open(file_path, 'r') as data:
+            r_data = json.load(data)
+        value = r_data[f"{material_name}"]["manufacturer_datasheet"][f"{property}"]
+        print(value)
+        return value
+
+    # ----------to get steinmetz data from database file-----------------------
+    @staticmethod
+    def get_steinmetz_data(material_name: str, type: str, datasource: str):
+        """
+        :param material_name:
+        :param datasource: measurement or datasheet
+        :param type: steinmetz or generalized steinmetz
+        """
+        script_dir = os.path.dirname(__file__)
+        file_path = os.path.join(script_dir, 'data/material_data_base.json')
+        with open(file_path, 'r') as data:
+            s_data = json.load(data)
+
+        s_data_new = s_data[f"{material_name}"][f"{datasource}"]
+        if type == "Steinmetz":
+            for i in range(len(s_data_new)):
+                if s_data_new[i]["data_type"] == "steinmetz_data":
+                    coefficient = dict(s_data_new[i]["data"])
+        # elif type == "Generalized_Steinmetz":
+        #     coefficient = dict(s_data[f"{material_name}"]["generalized_steinmetz_data"])
+        # print(coefficient)
+        return coefficient
+
 
 def compare_core_loss_flux_density_data(material_list: list, temperature: float = None):
     """
