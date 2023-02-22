ec159a99adbf8b4d3a4320e30ac16464cbc2420f
Nikolas Förster <foerster@lea.uni-paderborn.de>
2023-02-22 11:05:37 +0100

    add tests, typos

--- materialdatabase/function_tests/get_permeability_data_from_LEA_LK.py
+++ materialdatabase/function_tests/get_permeability_data_from_LEA_LK.py
@@ -1,13 +1,13 @@
 from materialdatabase.material_data_base_functions import *
 
 location = "C:/Users/tpiepe/sciebo/Exchange_FEMMT/05_Materials/data/2022_10_10_Ferrite_mu_eps_Data_Keuck/"
-filename = create_permeability_file_name_LEA_LK()
+filename = create_permeability_file_name_lea_lk()
 
 print(filename)
 for f in [100000, 200000, 300000, 400000, 500000]:
     for T in [30, 60, 80, 100]:
 
-        b_ref, mu_r, mu_phi_deg = get_permeability_data_from_LEA_LK(location, f, T, "N49")
+        b_ref, mu_r, mu_phi_deg = get_permeability_data_from_lea_lk(location, f, T, "N49")
 
         b_ref, mu_r, mu_phi_deg = process_permeability_data(b_ref, mu_r, mu_phi_deg,
                                                             smooth_data=True, crop_data=False, plot_data=True)
--- materialdatabase/function_tests/get_property_from_LEA_LK.py
+++ materialdatabase/function_tests/get_property_from_LEA_LK.py
@@ -4,13 +4,13 @@ from scipy.signal import savgol_filter as savgol
 
 location = "C:/Users/tpiepe/sciebo/Exchange_FEMMT/05_Materials/data/2022_10_10_Ferrite_mu_eps_Data_Keuck/"
 
-filename = create_permeability_file_name_LEA_LK()
+filename = create_permeability_file_name_lea_lk()
 
 print(filename)
 for f in [200000, 300000]:
-    b_hys, p_hys = get_permeability_property_from_LEA_LK(path_to_parent_folder=location, sub_folder_name="Core_Loss",
+    b_hys, p_hys = get_permeability_property_from_lea_lk(path_to_parent_folder=location, sub_folder_name="Core_Loss",
                                                          quantity="p_hys", frequency=f, material_name="N49", temperature=30)
-    b_phi, mu_phi_deg = get_permeability_property_from_LEA_LK(path_to_parent_folder=location, sub_folder_name="mu_phi_Plot",
+    b_phi, mu_phi_deg = get_permeability_property_from_lea_lk(path_to_parent_folder=location, sub_folder_name="mu_phi_Plot",
                                                               quantity="mu_phi", frequency=f, material_name="N49", temperature=30)
     if len(b_phi) != len(b_hys):
         print("invalid")
