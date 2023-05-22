6bd668f9883b5000417e255b2537ec703059e11a
Till Piepenbrock <piepenbrock@lea.uni-paderborn.de>
2023-05-22 11:00:19 +0200

    added permeability measurement of PC200 to database

--- materialdatabase/helper_functions/write_permeability_data_into_database.py
+++ materialdatabase/helper_functions/write_permeability_data_into_database.py
@@ -21,17 +21,18 @@ mdb = MaterialDatabase()
 # create_permeability_measurement_in_database("N49", "LEA_LK", company="Paderborn University", date="2021-04-29", test_setup_name="LEA_LK",
 #                                             toroid_dimensions="R25x21x16 mm", measurement_method="Electrical", equipment_names="Zimmer LMG640")
 
-create_permeability_measurement_in_database("N95", "LEA_LK", company="Paderborn University", date="2021-04-29", test_setup_name="LEA_LK",
-                                            toroid_dimensions="R15x21x16 mm", measurement_method="Electrical", equipment_names="Zimmer LMG640")
+create_permeability_measurement_in_database("PC200", "LEA_LK", company="Paderborn University", date="2021-04-29", test_setup_name="LEA_LK",
+                                            toroid_dimensions="???", measurement_method="Electrical", equipment_names="Zimmer LMG640")
 
-for f in [100000, 200000, 300000, 400000, 500000]:
+# for f in [100000, 200000, 300000, 400000, 500000]:
+for f in [500000]:
     for T in [30, 60, 80, 100]:
         print(f"{f, T =}")
-        b_ref, mu_r, mu_phi_deg = get_permeability_data_from_lea_lk(location, f, T, "N95")
+        b_ref, mu_r, mu_phi_deg = get_permeability_data_from_lea_lk(location, f, T, "PC200")
 
         b_ref, mu_r, mu_phi_deg = process_permeability_data(b_ref, mu_r, mu_phi_deg, smooth_data=True, crop_data=False, plot_data=True)
 
         # write_permeability_data_into_database(f, T, b_ref, mu_r, mu_phi_deg, "custom_material", "ABC")
         # write_permeability_data_into_database(f, T, b_ref, mu_r, mu_phi_deg, "N87", "LEA_LK")
-        write_permeability_data_into_database(f, T, b_ref, mu_r, mu_phi_deg, "N95", "LEA_LK")
+        write_permeability_data_into_database(f, T, b_ref, mu_r, mu_phi_deg, "PC200", "LEA_LK")
         # write_permeability_data_into_database(f, T, b_ref, mu_r, mu_phi_deg, "N49", "LEA_LK")
