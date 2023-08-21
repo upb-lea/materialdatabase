01125876fdd4911c4a109d41f595ad50093350e1
Sebastian Schachten <schacht@ad.uni-paderborn.de>
2023-08-21 15:58:56 +0200

    added some material enumerations

--- materialdatabase/enumerations.py
+++ materialdatabase/enumerations.py
@@ -27,13 +27,45 @@ class MeasurementDevice(str, Enum):
 
 
 class Material(str, Enum):
-    """Sets the type of Measurement Device
+    """Sets the type of Measurement Material
     """
+    _3F46 = "3F46"
+    N49 = "N49"
     N87 = "N87"
+    N95 = "N95"
+    PC200 = "PC200"
+    DMR96A = "DMR96A"
+    DMR96A2_test = "DMR96A2_test"
 
 
 class ToroidDirectoryName(str, Enum):
-    """Sets the type of Measurement Device
+    """Sets the type of Permeability Measurement Probe
+
+    string with:
+    d_out x d_in x h x N1 x N2
+
+    d_out: outer diameter of toroid in mm
+    d_in: innter diameter of toroid in mm
+    h: height of toroid in mm
+    N1: primary turns number
+    N2: secondary turns number
     """
     N87_1 = "R24,6x20,25x20,5_A00"
+    DMR96A_1 = '???'
+    DMR96A_2 = 'R_25x21x15x4x4'
+
+
+class CuboidDirectoryName(str, Enum):
+    """Sets the type of Permittivity Measurement Probe
+
+    string with:
+    a x b x c
+
+    a: width of cuboid in mm
+    b: thickness of cuboid in mm
+    c: height of cuboid in mm
+
+    (a and b can be exchanged, because A = a*b)
+    """
+    DMR96A_2 = "C_25x2x15"
 
