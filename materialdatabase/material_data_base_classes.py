656fe0d438d85f241e84fce32151d98d6f3a7c36
Nikolas Förster <foerster@lea.uni-paderborn.de>
2023-11-03 12:22:59 +0100

    fix random letter in code

--- materialdatabase/material_data_base_classes.py
+++ materialdatabase/material_data_base_classes.py
@@ -731,7 +731,7 @@ class MaterialDatabase:
             return self.data[material_name][datasource][datatype][measurement_setup]["measurement_data"]
         except:
             raise ValueError("Requested measurement data not available.")
-g
+
 
     def get_permittivity(self, temperature: float, frequency: float, material_name: str,
                          datasource: str = "measurements",
