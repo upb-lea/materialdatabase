cb9eb1207eae9c107b963eb42336064a1326755f
Sebastian Schachten <schacht@ad.uni-paderborn.de>
2024-05-24 10:47:59 +0200

    changed file format of README .md -> .rst updated README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """The setup script."""
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('CHANGELOG.md') as history_file:
