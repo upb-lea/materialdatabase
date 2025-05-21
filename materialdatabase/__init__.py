f7b1f0fa987cb07e8a0f87e2e5c790a7c463212f
Till Piepenbrock <piepenbrock@lea.uni-paderborn.de>
2025-05-21 08:17:20 +0200

    removed sample_file.py

--- materialdatabase/__init__.py
+++ materialdatabase/__init__.py
@@ -1,5 +1,4 @@
 """Initialize the material database package."""
 from materialdatabase.processing import *
 from materialdatabase.meta import *
-from materialdatabase.sample_file import *
 from materialdatabase.processing import *
