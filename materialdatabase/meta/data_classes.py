2146625aba9892b90c15d6403b4c655dc02b9acf
Till Piepenbrock <piepenbrock@lea.uni-paderborn.de>
2025-06-10 08:12:13 +0200

    add dataclass MaterialCurve

--- materialdatabase/meta/data_classes.py
+++ materialdatabase/meta/data_classes.py
@@ -1,5 +1,6 @@
 """Collection of dataclasses."""
 from dataclasses import dataclass
+import numpy as np
 
 from materialdatabase.meta.data_enums import Material, MeasurementSetup
 
@@ -14,3 +15,22 @@ class MaterialPlotConfig:
     label: str
     color: str
     marker: str
+
+
+@dataclass
+class MaterialCurve:
+    """
+    Stores material curves for one material together in this dataclass.
+
+    This dataclass can be loaded into a calculation, by only choosing the material.
+    All other parameters are bound to this material.
+    """
+
+    material_name: str
+    material_mu_r_abs: float
+    material_flux_density_vec: np.ndarray
+    material_mu_r_imag_vec: np.ndarray
+    material_mu_r_real_vec: np.ndarray
+    saturation_flux_density: float
+    boundary_temperature: float
+    boundary_frequency: float
