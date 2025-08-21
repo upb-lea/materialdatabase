48fc3b2c4136402bd193247db1d39f5637d8224b
gituser789 <62549000+gituser789@users.noreply.github.com>
2025-08-21 15:03:45 +0200

    rm unused dataclass

--- materialdatabase/meta/data_classes.py
+++ materialdatabase/meta/data_classes.py
@@ -24,21 +24,3 @@ class ComplexPermeabilityPlotConfig:
     color: str
     marker: str
 
-
-@dataclass
-class MaterialCurve:
-    """
-    Stores material curves for one material together in this dataclass.
-
-    This dataclass can be loaded into a calculation, by only choosing the material.
-    All other parameters are bound to this material.
-    """
-
-    material_name: str
-    material_mu_r_abs: float
-    material_flux_density_vec: np.ndarray
-    material_mu_r_imag_vec: np.ndarray
-    material_mu_r_real_vec: np.ndarray
-    saturation_flux_density: float
-    boundary_temperature: float
-    boundary_frequency: float
