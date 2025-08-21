54015dd5a48cb9e0060b6b47e049d61925bb480b
gituser789 <62549000+gituser789@users.noreply.github.com>
2025-08-21 16:53:34 +0200

    tests for examples

--- examples/get_datasheet_information.py
+++ examples/get_datasheet_information.py
@@ -6,26 +6,32 @@ import materialdatabase as mdb
 # configure logging to show terminal output
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-# init a material database instance
-mdb_data = mdb.Data()
-material_name = mdb.Material.N49
+def get_datasheet_information_example():
+    """get_datasheet_information() example."""
+    # init a material database instance
+    mdb_data = mdb.Data()
+    material_name = mdb.Material.N49
 
-resistivity = mdb_data.get_datasheet_information(material=material_name,
-                                                 attribute=mdb.DatasheetAttribute.Resistivity)
+    resistivity = mdb_data.get_datasheet_information(material=material_name,
+                                                     attribute=mdb.DatasheetAttribute.Resistivity)
 
-print(resistivity)
+    print(f"{resistivity=}")
 
-mu_initial = mdb_data.get_datasheet_information(material=material_name,
-                                                attribute=mdb.DatasheetAttribute.InitialPermeability)
+    mu_initial = mdb_data.get_datasheet_information(material=material_name,
+                                                    attribute=mdb.DatasheetAttribute.InitialPermeability)
 
-print(mu_initial)
+    print(f"{mu_initial=}")
 
-bsat = mdb_data.get_datasheet_information(material=material_name,
-                                          attribute=mdb.DatasheetAttribute.SaturationFluxDensity100)
+    bsat = mdb_data.get_datasheet_information(material=material_name,
+                                              attribute=mdb.DatasheetAttribute.SaturationFluxDensity100)
 
-print(bsat)
+    print(f"{bsat=}")
 
-density = mdb_data.get_datasheet_information(material=material_name,
-                                             attribute=mdb.DatasheetAttribute.Density)
+    density = mdb_data.get_datasheet_information(material=material_name,
+                                                 attribute=mdb.DatasheetAttribute.Density)
 
-print(density)
+    print(f"{density=}")
+
+
+if __name__ == "__main__":
+    get_datasheet_information_example()
---
+++ tests/example_test.py
@@ -0,0 +1,6 @@
+
+
+from examples.get_datasheet_curve import get_datasheet_curve_example
+
+
+get_datasheet_curve_example()
