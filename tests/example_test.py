c1804c202bfb623a0222db23ca7c7cbd4307bdd1
gituser789 <62549000+gituser789@users.noreply.github.com>
2025-08-21 16:59:08 +0200

    linting

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,6 +1,7 @@
-
+"""Tests for the example files."""
 
 from examples.get_datasheet_curve import get_datasheet_curve_example
-
+from examples.get_datasheet_information import get_datasheet_information_example
 
 get_datasheet_curve_example()
+get_datasheet_information_example()
